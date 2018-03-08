00dba587b17f0b379df569ca16e8791ca4b3c1b6
Elise <eliseakemi@gmail.com>
2018-03-08 13:44:08 -0300

    Inicializando projeto

---
+++ Domain/Produto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
