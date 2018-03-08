d3ac505bb120edf9cc1fb2de1c0658cbde79ecfb
Elise <eliseakemi@gmail.com>
2018-03-08 15:58:12 -0300

    Adicionado implementação do DbContext, que permite a interação do Domain com o DB.

---
+++ Data/ProductDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext() : base("Name=ProductDb") // Passa por parâmetro o nome da connectionString
+        {
+            // Inicializa tabela no banco 
+            Database.SetInitializer<ProductDbContext>(new CreateDatabaseIfNotExists<ProductDbContext>());
+        }
+
+    }
+}
