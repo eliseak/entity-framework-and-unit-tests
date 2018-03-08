2a89d4c3edb58d4e849e60703bf52b3b4390545c
Elise <eliseakemi@gmail.com>
2018-03-08 16:14:31 -0300

    Adicionar DbSet de Produto

--- Data/ProductDbContext.cs
+++ Data/ProductDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -9,11 +10,13 @@ namespace Data
 {
     public class ProductDbContext : DbContext
     {
+        public DbSet<Produto> Produtos { get; set; }
+
         public ProductDbContext() : base("Name=ProductDb") // Passa por parâmetro o nome da connectionString
         {
             // Inicializa tabela no banco 
             Database.SetInitializer<ProductDbContext>(new CreateDatabaseIfNotExists<ProductDbContext>());
         }
-
+        
     }
 }
