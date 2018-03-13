9f6a22d5a678e08ba74da8e84ff2bd4fd0f03910
Elise <eliseakemi@gmail.com>
2018-03-13 12:47:44 -0300

    Adicionado relacionamento

--- Domain/Loja.cs
+++ Domain/Loja.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain
 {
+
+    [Table ("Loja")] //Data notation: especifica o nome da tabela
     public class Loja
     {
+        public Loja()
+        {
+            Produtos = new List<Produto>();
+        }
+
+        [Key]
         public int Id { get; set; }
+
+        [MaxLength(200)]
+        [Required]
         public string Nome { get; set; }
+        [MaxLength(2000)]
         public string Descricao { get; set; }
+
+        public virtual IEnumerable<Produto> Produtos { get; set; }
     }
 }
