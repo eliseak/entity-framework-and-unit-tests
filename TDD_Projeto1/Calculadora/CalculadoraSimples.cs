03fb3d75c7d7c7a2b91ee745c5b503e06df1c216
Elise <eliseakemi@gmail.com>
2018-04-03 10:31:42 -0300

    Estudos de TDD: criado classe para ser testada

---
+++ TDD_Projeto1/Calculadora/CalculadoraSimples.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Calculadora
+{
+    public class CalculadoraSimples
+    {
+        // Adiciona dois números
+        public int Adicionar (int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        // Multiplica dois números
+        public int Multiplicar(int n1, int n2)
+        {
+            // Demo:
+            return n1 + n2; 
+        }
+    }
+}
